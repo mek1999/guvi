95ba627e6847445d91f7280814b3564a04db90f9
mek1999 <47376832+mek1999@users.noreply.github.com>
2019-02-06 22:48:06 +0530

    Create leapyearornot.java

---
+++ leapyearornot.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+class Check_Leap_Year 
+{
+    public static void main(String args[])
+    {
+        Scanner s = new Scanner(System.in);
+        int year = s.nextInt();
+        boolean flag = false;
+        if(year % 400 == 0)
+        {
+            flag = true;
+        }
+        else if (year % 100 == 0)
+        {
+            flag = false;
+        }
+        else if(year % 4 == 0)
+        {
+            flag = true;
+        }
+        else
+        {
+            flag = false;
+        }
+        if(flag)
+        {
+            System.out.println("yes");
+        }
+        else
+        {
+            System.out.println("no");
+        }
+    }
+}
