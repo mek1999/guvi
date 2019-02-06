c69d4f64617c4215c727dbb84a7e1e93ebd43d8d
mek1999 <47376832+mek1999@users.noreply.github.com>
2019-02-06 22:23:12 +0530

    Create helloforn.java

---
+++ helloforn.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class helloprogram
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int a,i=0;
+		Scanner s=new Scanner(System.in);
+		a=s.nextInt();
+		while(i<a)
+		{
+			System.out.println("Hello");
+			i++;
+		}
+	}
+}
