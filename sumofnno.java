b9ce8bf3cfd93377bd94bd47f7283a15a03d2c3d
mek1999 <47376832+mek1999@users.noreply.github.com>
2019-02-06 22:37:58 +0530

    Create sumofnno.java

---
+++ sumofnno.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n, sum = 0,i;
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+	    for (i = 1;i<= n;i++)
+		{
+			 sum = sum + i;
+		}
+		System.out.println(+sum);
+ 	}
+}
