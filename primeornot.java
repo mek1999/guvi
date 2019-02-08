8447d1174ba1b572322006ce79293fa1dba3f1d8
mek1999 <47376832+mek1999@users.noreply.github.com>
2019-02-08 23:06:30 +0530

    Create primeornot.java

---
+++ primeornot.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{ 
+		int num;
+        boolean flag = false;
+        Scanner s=new Scanner(System.in);
+        num=s.nextInt();
+        for(int i = 2; i <= num/2; ++i)
+        {
+            if(num % i == 0)
+            {
+                flag = true;
+                break;
+            }
+        }
+
+        if (!flag)
+        {
+            System.out.println("yes");
+        }
+        else
+        {
+            System.out.println("no");
+        }
+	}
+}
+
