afa52a1b5e0dbabd7fef5fd0a600c5520f27a0a8
mek1999 <47376832+mek1999@users.noreply.github.com>
2019-02-08 22:41:51 +0530

    Create powerofvalue.java

---
+++ powerofvalue.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int number, i, exponent;
+		long power = 1;
+		Scanner sc = new Scanner(System.in);
+		number = sc.nextInt();	
+		exponent = sc.nextInt();	
+		for(i = 1; i <= exponent; i++)
+		{
+			power = power * number;
+		}
+		System.out.println(+power);
+	}
+}
+
