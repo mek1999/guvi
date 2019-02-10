5efade3369237654027a4ae5831a219071559d48
mek1999 <47376832+mek1999@users.noreply.github.com>
2019-02-10 17:44:22 +0530

    Create evennob|w2integer.java

---
+++ evennob|w2integer.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{	
+		int i,a,b;
+		Scanner s=new Scanner(System.in);
+		a=s.nextInt();
+		b=s.nextInt();
+		for (i=a+1;i<b;i++) 
+		{
+			if (i%2==0) 
+			{
+				System.out.print(i+" ");
+			}
+		}
+	}
