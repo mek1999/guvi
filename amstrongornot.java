363b5b2a2f21cd4cddb94f26a7f8fde6f69cccef
mek1999 <47376832+mek1999@users.noreply.github.com>
2019-02-10 19:32:43 +0530

    Create amstrongornot.java

---
+++ amstrongornot.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	  int c=0,a,temp;  
+    int n;
+    Scanner s=new Scanner(System.in);
+    n=s.nextInt();
+    temp=n;  
+    while(n>0)  
+    {  
+       a=n%10;  
+       n=n/10;  
+       c=c+(a*a*a);  
+    }  
+    if(temp==c)  
+    {
+    	System.out.println("yes");   
+    }
+    else  
+     {
+     	System.out.println("no");  
+     }
+	}
+}
