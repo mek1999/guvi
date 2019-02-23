651a2490a4eb89593972e9ee65d4c4c5e31f39f5
mek1999 <47376832+mek1999@users.noreply.github.com>
2019-02-23 02:43:55 -0800

    Create multipleofnnumber.java

---
+++ multipleofnnumber.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int a,n;
+		Scanner s=new Scanner(System.in);
+		a=s.nextInt();
+		for(int i=1;i<=5;i++)
+		{
+			n=i*a;
+			System.out.print(n + " ");
+		}
+	}
+}
+          
