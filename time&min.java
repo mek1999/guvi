68ca8e7a5330ff422945fa7a28308bc3acead3b6
mek1999 <47376832+mek1999@users.noreply.github.com>
2019-02-24 15:32:38 +0530

    Create time&min.java

---
+++ time&min.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int m,n,l;
+		Scanner s = new Scanner(System.in);
+        m = s.nextInt();
+        n=m/60;
+        l=m%60;
+        System.out.println(""+n+" "+l);
+        
+	}
+}
