f408ea4bf46e7dd486dcd844619b606c9d78fff7
mek1999 <47376832+mek1999@users.noreply.github.com>
2019-02-24 15:22:51 +0530

    Create stringisnumeric.java

---
+++ stringisnumeric.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		String n;
+        Scanner s = new Scanner(System.in);
+        n = s.nextLine();
+       boolean numeric = true;
+       try
+       {
+            Double num = Double.parseDouble(n);
+        } 
+        catch (NumberFormatException e) 
+        {
+            numeric = false;
+        }
+
+        if(numeric)
+        {
+            System.out.println( "yes");
+        }
+        else
+        {
+            System.out.println("no");
+        }
+	}
+	
+}
