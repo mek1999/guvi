a21292fcc3533142a68e040f3e90d0fadd0b3904
mek1999 <47376832+mek1999@users.noreply.github.com>
2019-03-05 22:18:48 +0530

    Create swapbytemp.java

---
+++ swapbytemp.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int a,b,temp;
+		Scanner s=new Scanner(System.in);
+		a=s.nextInt();
+		b=s.nextInt();
+		temp=a;
+		a=b;
+		b=temp;
+		System.out.println(""+a+" "+b);
+		
+	}
+}
