f9a5e3edbcdf90a72e6db62c832bad54938023f6
mek1999 <47376832+mek1999@users.noreply.github.com>
2019-03-09 20:15:22 +0530

    Create addtwonumber.java

---
+++ addtwonumber.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+        int a,sum=0,b;
+        Scanner s=new Scanner(System.in);
+        a=s.nextInt();
+        b=s.nextInt();
+        System.out.println(""+(a+b));
+	}
+}
