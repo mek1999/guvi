23da267913183b22f0ecdd23d59a7f093c95b54c
mek1999 <47376832+mek1999@users.noreply.github.com>
2019-03-09 13:19:37 +0530

    Create stringatntomes.java

---
+++ stringatntomes.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+    public static void main(String args[])
+    {
+        int k;
+        String str;
+        Scanner s = new Scanner(System.in);
+        str= s.next();
+        k= s.nextInt();
+        for(int i=0;i<k;i++)
+        {
+        	System.out.println(""+str);
+        }
+    }
+}
