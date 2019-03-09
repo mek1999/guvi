5cbfb631807418f35c7f5030ea1e8b1a2126c397
mek1999 <47376832+mek1999@users.noreply.github.com>
2019-03-09 15:54:57 +0530

    Create rangefrom1to10.java

---
+++ rangefrom1to10.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+    public static void main(String args[])
+    {
+        int n;
+        Scanner s = new Scanner(System.in);
+        n=s.nextInt();
+        if(n>=1&&n<=10)
+        {
+        	System.out.print("yes");
+        }
+        else
+        {
+			System.out.print("no");
+        	
+        }
+        
+    }
+}
