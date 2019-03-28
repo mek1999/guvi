3f41cd0872dd3e3e3b2388528d86e09b8625bbe5
mek1999 <47376832+mek1999@users.noreply.github.com>
2019-03-28 22:05:07 +0530

    Create strbufferreverse.java

---
+++ strbufferreverse.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+        StringBuffer sb= new StringBuffer();
+		    Scanner input = new Scanner(System.in);
+	    	sb.append(input.nextLine());
+	    	sb.reverse();
+	    	System.out.println(""+sb);
+	}
+}
