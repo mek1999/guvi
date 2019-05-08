dd2131cce3206b42c7bb9bdd26d5c8527a908844
mek1999 <47376832+mek1999@users.noreply.github.com>
2019-05-08 14:18:23 +0530

    Create numberintodigit.java

---
+++ numberintodigit.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Aver
+{
+    public static void main(String aa[])
+    {
+     String a;
+     long n;
+     Scanner s=new Scanner(System.in);
+     n=s.nextLong();
+     a=String.valueOf(n);
+     char b[]=a.toCharArray();
+     for(int i=0;i<b.length;i++)
+	 {
+         System.out.print(b[i]+" ");
+		
+     } 
+	}
+}
