0182c4359f4322a657c27c0aee70c694837fb279
mek1999 <47376832+mek1999@users.noreply.github.com>
2019-05-13 22:04:30 +0530

    Create day yes or no.java

---
+++ day yes or no.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		String a;
+		a=s.next();
+		if((aequalto"Saturday")||(a=="Sunday"))
+		{
+			System.out.print("yes");
+		}
+		else
+		{
+			System.out.print("no");
+		}
+	}
+}
