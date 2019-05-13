4a8782a14106dac97f0144044929bb9b01dfbc71
mek1999 <47376832+mek1999@users.noreply.github.com>
2019-05-13 19:38:08 +0530

    Create product is evenodd.java

---
+++ product is evenodd.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int n,m,a;
+		n=s.nextInt();
+		m=s.nextInt();
+		a=n*m;
+		if(a%2!=0)
+		{
+			System.out.print("odd");
+		}
+		else
+		{
+			System.out.print("even");
+		}
+	}
+}
