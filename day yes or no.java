31f5c525ca6ede4f6e942cc183db30f399fdaa67
mek1999 <47376832+mek1999@users.noreply.github.com>
2019-05-22 16:44:12 +0530

    Update day yes or no.java

--- day yes or no.java
+++ day yes or no.java
@@ -1,14 +1,16 @@
 import java.util.*;
 import java.lang.*;
 import java.io.*;
-class Ideone
+public class day_yes_or_no
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		Scanner s=new Scanner(System.in);
+		String day1="Saturday";
+		String day2="Sunday";
 		String a;
 		a=s.next();
-		if((aequalto"Saturday")||(a=="Sunday"))
+		if((a.equals(day1))||(a.equals(day2)))
 		{
 			System.out.print("yes");
 		}
