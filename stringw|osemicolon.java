1295b3305d07edf92cac6dab3bf5e14d83127c48
mek1999 <47376832+mek1999@users.noreply.github.com>
2019-06-03 19:30:27 +0530

    Create stringw|osemicolon.java

---
+++ stringw|osemicolon.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		String s;
+		s=sc.nextLine();
+		for(int i = 1; i < 2; System.out.println(""+s))
+    {
+            i++;
+   	}
+	}
+	
+}
