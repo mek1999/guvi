ee67cca34144a525d25366b7c83eadb8e94fd77a
mek1999 <47376832+mek1999@users.noreply.github.com>
2019-06-03 19:57:35 +0530

    Create nearestgreatmuloften.java

---
+++ nearestgreatmuloften.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+public class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int a,b;
+		a=sc.nextInt();
+		b=((a/10)*10);
+		System.out.print(""+(b+10));
+	}
+	
+}
