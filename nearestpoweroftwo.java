5efe30880f1ec0faa5d47ef8cff0763e6cf89d21
mek1999 <47376832+mek1999@users.noreply.github.com>
2019-06-03 21:56:30 +0530

    Create nearestpoweroftwo.java

---
+++ nearestpoweroftwo.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+public class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int a;
+		a=sc.nextInt();
+		if(a%2==0)
+		{
+			System.out.print(""+(a*2));
+		}
+	}
+	
+}
