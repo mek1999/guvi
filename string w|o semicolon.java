6b9aaeeaf660e125b2b411f948970d9646c8aff4
mek1999 <47376832+mek1999@users.noreply.github.com>
2019-06-03 19:28:50 +0530

    Create string w|o semicolon.java

---
+++ string w|o semicolon.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+public class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		String s;
+		s=sc.nextLine();
+		if(System.out.printf(""+s) == null) {}
+	}
+	
+}
