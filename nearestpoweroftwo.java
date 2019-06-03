6ea17f5bf6bbc88e892dce08850fd5f413a04d00
mek1999 <47376832+mek1999@users.noreply.github.com>
2019-06-03 22:00:48 +0530

    Update nearestpoweroftwo.java

--- nearestpoweroftwo.java
+++ nearestpoweroftwo.java
@@ -8,10 +8,7 @@ public class Ideone
 		Scanner sc=new Scanner(System.in);
 		int a;
 		a=sc.nextInt();
-		if(a%2==0)
-		{
-			System.out.print(""+(a*2));
-		}
+		System.out.print(""+(a*2));
 	}
 	
 }
