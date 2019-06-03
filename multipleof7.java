2bc949f38eb58850ef3757e5eee4698aab3233a2
mek1999 <47376832+mek1999@users.noreply.github.com>
2019-06-03 22:14:18 +0530

    Create multipleof7.java

---
+++ multipleof7.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+public class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n;
+		n=sc.nextInt();
+		if(n%7==0)
+		{
+			System.out.print("yes");
+		}
+		else
+		{
+			System.out.print("no");
+		}
+	}
+	
+}
