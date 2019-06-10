53d9918c0f9eb6a065b8c40d6ae381233cba935e
mek1999 <47376832+mek1999@users.noreply.github.com>
2019-06-10 13:08:18 +0530

    Create decimaltonearnum.java

---
+++ decimaltonearnum.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+public class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{ 
+		Scanner s=new Scanner(System.in);
+		double a,b;
+		a=s.nextDouble();
+		b=a+1.0;
+		int i;
+		i=(int)b;
+		System.out.print(""+i);
+	}
+}
