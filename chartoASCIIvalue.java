5656f64e984a710c0d5394b77fd261fd21adff3e
mek1999 <47376832+mek1999@users.noreply.github.com>
2019-06-11 19:34:16 +0530

    Create chartoASCIIvalue.java

---
+++ chartoASCIIvalue.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+public class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{	
+		char a;
+		Scanner sc = new Scanner(System.in);
+		a=sc.next().charAt(0); 
+		int n=(int)a;
+		System.out.print(n);
+	}
+}
