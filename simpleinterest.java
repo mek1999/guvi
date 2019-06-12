fd7df5b469382ae8b0ec59592e30cd49d79434b9
mek1999 <47376832+mek1999@users.noreply.github.com>
2019-06-12 12:52:31 +0530

    Create simpleinterest.java

---
+++ simpleinterest.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+        Scanner s=new Scanner(System.in);
+		int p,t,r,si;
+		p=s.nextInt();
+		t=s.nextInt();
+		r=s.nextInt();
+		si=(p*t*r)/100;
+		System.out.print(si);
+	}
+}
