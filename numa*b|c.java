dfeddac416f3cadef98da94c59e170ac726b4764
mek1999 <47376832+mek1999@users.noreply.github.com>
2019-06-12 20:52:02 +0530

    Create numa*b|c.java

---
+++ numa*b|c.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int a,b,c,m;
+		a=s.nextInt();
+		b=s.nextInt();
+		c=s.nextInt();
+		m=((a*b)/c);
+		System.out.print(m);
+	}
+}
