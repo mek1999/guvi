50ae99c98b243e01361213d1a0512fe7cbee5943
mek1999 <47376832+mek1999@users.noreply.github.com>
2019-06-12 19:05:54 +0530

    Create celciustokelvin.java

---
+++ celciustokelvin.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+        Scanner s=new Scanner(System.in);
+		double c,k;
+		c=s.nextInt();
+		k=c+273.15;
+		System.out.print(k);
+	}
+}
