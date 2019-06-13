61312f4a5d714d6c2bf2236057efa5a5f0e206aa
mek1999 <47376832+mek1999@users.noreply.github.com>
2019-06-13 18:14:06 +0530

    Create pow of n,k.java

---
+++ pow of n,k.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		double n,k,m;
+		n=s.nextDouble();
+		k=s.nextDouble();
+		m=Math.pow(n,k);
+		long l;
+		l=(long)m;
+		System.out.print(l);
+	}
+}
